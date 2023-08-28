f2ba722b2376bec2a4a8aef8e4c2f1bcf44fd86e
Delo-O-Part5 <didehw@gmail.com>
2023-08-27 23:32:55 -0400

    Updated verion

--- temperature_convertor.c
+++ temperature_convertor.c
@@ -9,19 +9,26 @@ int main(){
     printf("\nIs the temperature in (F) or (C): ");
     scanf("%c", &unit);
 
+    unit = toupper(unit);
+
     if (unit == 'C'){
-        printf("The temperature presently in C");
+        printf("\nEnter the temperature in Celsius: ");
+        scanf("%f",&temp);
+        temp = (temp * 9 / 5) + 32;
+        printf("\nThe temperature in Fahrenheit is: %.1f", temp);
     }
 
     else if (unit == 'F'){
-        printf("The temperature presently in F");
+        printf("\nEnter the temperature in Fahrenheit: ");
+        scanf("%f",&temp);
+        temp = ((temp - 32) * 5) / 9;
+        printf("\nThe temperature in Celsius is: %.1f", temp); 
     }
 
     else {
         printf("\nThis is not a unit");
     }
 
-
-    return 0;
+  return 0;
     
 }
