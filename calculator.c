a0cf4a822e26b90f9e849a1d23ff698525f6ac04
Delo-O-Part5 <didehw@gmail.com>
2023-09-15 19:51:58 -0400

    Mini calculator

---
+++ calculator.c
@@ -0,0 +1,46 @@
+# include <stdio.h>
+
+
+int main(){
+
+char operator;
+double numb1;
+double numb2;
+double result;
+
+printf("\nEnter the an operation (+, -, *, /): ");
+scanf("%c", &operator);
+
+printf("\nEnter the first number: ");
+scanf("%lf",&numb1);
+
+printf("\nEnter the second number: ");
+scanf("%lf",&numb2);
+
+switch (operator){
+    
+    case '+':
+        result = numb1 + numb2;
+        printf("\nThe result: %lf",result);
+        break;
+
+    case '-':
+        result = numb1 - numb2;
+        printf("\nThe result: %lf",result);
+        break;
+
+    case '*': 
+        result = numb1 * numb2;
+        printf("\nThe result: %lf",result);
+        break;
+
+    case '/':
+        result = numb1 / numb2;
+    
+    default: 
+        printf("\n%c is not a valid operator", operator);
+}
+
+
+    return 0;
+}
